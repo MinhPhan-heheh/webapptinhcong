3d959b8ac8a757a82606b70fb5ddde442e2dc064
MinhPhan-heheh <minhphanheheh@gmail.com>
2026-05-25 08:58:20 +0700

    update api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://workshift-o5sm.onrender.com",
 });
 
 api.interceptors.request.use(
