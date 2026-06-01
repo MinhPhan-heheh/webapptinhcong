b28b89d1713eae3ef59b9812702588028543d940
MinhPhan-heheh <minhphanheheh@gmail.com>
2026-06-01 09:41:57 +0700

    fix: use Brevo SMTP for email sendin1g

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -10,8 +10,8 @@ const sendEmail = async (to, subject, html) => {
     
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.BREVO_API_KEY,
