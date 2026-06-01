5f1e6a97906da6476ef9334c74863667a4ec2a17
MinhPhan-heheh <minhphanheheh@gmail.com>
2026-06-01 10:37:51 +0700

    fix: change BrowserRouter to HashRouter to fix reload issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';  // Thêm dòng này
 
 // Auth pages
 import Login from "./pages/auth/Login";
